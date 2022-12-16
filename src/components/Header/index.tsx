1873f3a2f6fa9a0a2394e22b6e2f10a0d2a4c8e9
Gerson Rocha <gersonrocha9@gmail.com>
2022-12-15 23:03:20 -0300

    fix: removing functions on Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,45 +1,11 @@
-import { useState } from "react";
 import logo from "../../assets/images/logo.svg";
-import { CreateCategoryModal } from "../CreateCategoryModal";
-import { CreateProductModal } from "../CreateProductModal";
 
 import { Container, Content } from "./styles";
 
 export function Header() {
-  const [isCreateProductOpenModal, setIsCreateProductOpenModal] =
-    useState(false);
-  const [isCreateCategoryOpenModal, setIsCreateCategoryOpenModal] =
-    useState(false);
-
-  function handleCreateProductOpenModal() {
-    setIsCreateProductOpenModal(true);
-  }
-
-  function handleCreateProductCloseModal() {
-    setIsCreateProductOpenModal(false);
-  }
-
-  function handleCreateCategoryOpenModal() {
-    setIsCreateCategoryOpenModal(true);
-  }
-
-  function handleCreateCategoryCloseModal() {
-    setIsCreateCategoryOpenModal(false);
-  }
-
   return (
     <Container>
       <Content>
-        <CreateProductModal
-          isOpen={isCreateProductOpenModal}
-          onClose={handleCreateProductCloseModal}
-        />
-
-        <CreateCategoryModal
-          isOpen={isCreateCategoryOpenModal}
-          onClose={handleCreateCategoryCloseModal}
-        />
-
         <img src={logo} alt="WAITERAPP Logo" />
       </Content>
     </Container>
